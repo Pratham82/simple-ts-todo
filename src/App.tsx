13e8b243dff6c896349065d1f36095d2bac0bc11
Pratham82 <mali.prathamesh82@gmail.com>
2020-10-18 01:42:29 +0530

    Completed app and added base files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import AddTodoForm from "./components/AddTodoForm";
+import "./styles/Todo.css";
 
-function App() {
+const initialtodos: Array<Todo> = [
+  { text: "Complete ts tutorial", complete: false },
+  { text: "Complete React tutorials", complete: false },
+];
+
+console.log(initialtodos.map((v) => v.complete));
+const App: React.FC = () => {
+  // State management with hooks
+  const [todos, setTodos] = useState(initialtodos);
+
+  // Change todos
+  const toggleTodo: toggleTodo = (selectedTodo) => {
+    const newTodos = todos.map((todo) =>
+      todo === selectedTodo ? { ...todo, complete: !todo.complete } : todo
+    );
+    setTodos(newTodos);
+  };
+
+  const addTodo: AddTodo = (newTodo) => {
+    newTodo.trim() && setTodos([...todos, { text: newTodo, complete: false }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="innerContainer">
+        <h2 className="title">
+          {" "}
+          <i className="devicon-typescript-plain colored"></i> Todo App
+        </h2>
+        <AddTodoForm addTodo={addTodo} />
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
